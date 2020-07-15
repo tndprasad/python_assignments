36b7533683d226af5dfb2a6d1fda58d94c9245d6
tndprasad <67746964+tndprasad@users.noreply.github.com>
2020-07-15 13:38:38 +0530

    Add files via upload

---
+++ contact_form.py
@@ -0,0 +1,17 @@
+name = input("Name:")
+age = input("Age:")
+course = input("Course:")
+contact = input("Phone:")
+print("The name you have entered is :",name)
+print("")
+print("The age you have entered is :",age)
+print("")
+print("The course you have entered is :",course)
+print("")
+print("Your phone number is :",contact)
+print("")
+i = int(input("To confirm enter 1:"))
+if i == 1 :
+    print("Welcome to Netzwerk Academy")
+else :
+    print("PLease enter the correct inputs")
